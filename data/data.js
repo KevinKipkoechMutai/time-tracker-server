6e02d6a20645da8bfd6f48416c1010c96c7029fc
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-08 14:40:39 +0300

    Add seed data

---
+++ data/data.js
@@ -0,0 +1,18 @@
+export const tasks = [
+    {
+        name: "Fix bug #1234",
+        taskType: "bug",
+        timeSpent: 2,
+        startDate: new Date("2023-07-01"),
+        endDate: new Date("2023-07-02"),
+        labels: ["bug", "critical"],
+    },
+    {
+        name: "Add new feature",
+        taskType: "story",
+        timeSpent: 3,
+        startDate: new Date("2023-07-03"),
+        endDate: new Date("2023-07-06"),
+        labels: ["feature", "high priority"],
+    },
+];
